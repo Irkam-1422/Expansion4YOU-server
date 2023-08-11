090b5c4ddf9ad2ce96beadec3beebadee21fd009
Dmitry Mormul <macairmormul@MacBook-Air-Dmitry.local>
2023-08-11 21:59:21 +0200

    changed scripts

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get("/", (req,res) => {
-    res.send("Egnite your business growth!");
+    res.send("Ignite your business growth!");
     })
     
 module.exports = router 
