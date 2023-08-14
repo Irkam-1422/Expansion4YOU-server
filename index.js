4d9e4b67b8c8ef1b63593f8fba094231f12500e3
Dmitry Mormul <macairmormul@MacBook-Air-Dmitry.local>
2023-08-14 17:50:32 +0200

    still trying to get client files from server side

--- index.js
+++ index.js
@@ -18,20 +18,26 @@ const app = express()
 
 app.use(express.json({extended: true}))
 app.use(cors(corsOptions));
-// app.use(route)
+
+//app.use(route)
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/article', require('./routes/article.routes'))
 app.use('/api/content', require('./routes/content.routes'))
 app.use('/api/casestudy', require('./routes/casestudy.routes'))
 app.use('/api/email', require('./routes/email.routes'))  
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+// if (process.env.NODE_ENV === 'production') {
+//     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 async function start() { 
     try {
