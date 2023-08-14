936946dd816c64f249e02374778ed5c8a9b9b86b
Dmitry Mormul <macairmormul@MacBook-Air-Dmitry.local>
2023-08-14 17:43:31 +0200

    added NODE_ENV

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.use('/api/content', require('./routes/content.routes'))
 app.use('/api/casestudy', require('./routes/casestudy.routes'))
 app.use('/api/email', require('./routes/email.routes'))  
 
-// if (process.env.NODE_ENV === 'production') {
-    // app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    // })
-// }
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 async function start() { 
     try {
